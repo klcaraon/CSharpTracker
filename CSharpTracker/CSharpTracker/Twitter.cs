2b66858210603e9837c92009e5bad4e98ddce851
Kenneth Valle <kennethjacobvalle.pup@gmail.com>
2020-10-26 15:26:58 +0800

    [Twitter] Add OAuth Authentication Methods

--- CSharpTracker/CSharpTracker/Twitter.cs
+++ CSharpTracker/CSharpTracker/Twitter.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using System.Diagnostics;
+using Tweetinvi;
+using Tweetinvi.Models;
+using System.IO;
+using Tweetinvi.Parameters;
 
 namespace CSharpTracker
 {
@@ -14,6 +18,9 @@ namespace CSharpTracker
         private string _appTokenSecret;
         private string _consumerKey;
         private string _consumerSecretKey;
+        private string _bearerToken;
+        private TwitterClient twitterClient;
+
 
         public string ConsumerKey
         {
@@ -43,9 +50,51 @@ namespace CSharpTracker
         {
             _appToken = ConfigurationManager.AppSettings["TwitterAppToken"];
             _appTokenSecret = ConfigurationManager.AppSettings["TwitterAppTokenSecret"];
+            _bearerToken = ConfigurationManager.AppSettings["TwitterBearerToken"];
 
             Debug.WriteLine(_appToken);
             Debug.WriteLine(_appTokenSecret);
+
+            twitterClient = new TwitterClient(_consumerKey, _consumerSecretKey, _appToken, _appTokenSecret);
         }
+
+        #region Authentication
+        public async void AuthenticationRequest()
+        {
+            if (twitterClient == null)
+                throw new NullReferenceException("Twitter client is null");
+
+            var authenticationRequest = await twitterClient.Auth.RequestAuthenticationUrlAsync();
+
+            Process.Start(new ProcessStartInfo(authenticationRequest.AuthorizationURL)
+            {
+                UseShellExecute = true
+            });
+        }
+
+        public async void AuthenticationReceive(string pinCode, IAuthenticationRequest authenticationRequest)
+        {
+            if (twitterClient == null)
+                throw new NullReferenceException("Twitter client is null");
+
+            var userCredentials = await twitterClient.Auth.RequestCredentialsFromVerifierCodeAsync(pinCode, authenticationRequest);
+        }
+
+        private async Task<bool> CreateUserClient(ITwitterCredentials userCredentials)
+        {
+            twitterClient = new TwitterClient(userCredentials);
+            var user = await twitterClient.Users.GetAuthenticatedUserAsync();
+
+            ConsumerKey = userCredentials.AccessToken;
+            ConsumerSecretKey = userCredentials.AccessTokenSecret;
+
+            if (twitterClient == null)
+                return false;
+
+            return true;
+        }
+        #endregion
+
+
     }
 }
